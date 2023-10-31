d81bc42bb420986699296bbc091943d9fc8a9ec4
ChickenchickenLove <ojt90902@naver.com>
2023-10-31 09:56:09 +0900

    hard 2 with java

--- src/main/java/org/example/hard/lc123/Solution.java
+++ src/main/java/org/example/hard/lc123/Solution.java
@@ -1,2 +1,35 @@
-package org.example.hard.lc123;public class Solution {
+package org.example.hard.lc123;
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+
+        int[] left = new int[n];
+        int minimumPrice = Integer.MAX_VALUE; int maxValue = 0;
+        for (int i = 0; i < n; i++) {
+            int nowPrice = prices[i];
+            minimumPrice = Math.min(nowPrice, minimumPrice);
+            maxValue = Math.max(maxValue, nowPrice - minimumPrice);
+            left[i] = maxValue;
+        }
+
+        int[] right = new int[n];
+        int maxRight = 0; int maxValueRight = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            maxRight = Math.max(maxRight, prices[i]);
+            maxValue = Math.max(maxValueRight, maxRight - prices[i]);
+            right[i] = maxValue;
+        }
+
+        int result = Math.max(left[0], left[n - 1]);
+        result = Math.max(result, right[0]);
+        result = Math.max(result, right[n - 1]);
+
+
+        for (int i = 0; i < n-1; i++) {
+            result = Math.max(result, left[i] + right[i + 1]);
+        }
+
+        return result;
+    }
 }
--- src/main/java/org/example/hard/lc256/Solution.java
+++ src/main/java/org/example/hard/lc256/Solution.java
@@ -1,2 +1,28 @@
-package org.example.hard.lc256;public class Solution {
+package org.example.hard.lc256;
+
+import java.util.Arrays;
+
+class Solution {
+    public int minCostII(int[][] costs) {
+        final int house = costs.length;
+        final int color = costs[0].length;
+        final int[][] dp = new int[house][color];
+
+        for (int i = 0; i < color; i++) {
+            dp[0][i] = costs[0][i];
+        }
+
+        for (int i = 1; i < house; i++) {
+            for (int j = 0; j < color; j++) {
+                int previousMin = Integer.MAX_VALUE;
+                for (int k = 0; k < color; k++) {
+                    if (j == k ) continue;
+                    previousMin = Math.min(previousMin, dp[i - 1][k]);
+                }
+                dp[i][j] = previousMin + costs[i][j];
+            }
+        }
+
+        return Arrays.stream(dp[house - 1]).min().getAsInt();
+    }
 }
