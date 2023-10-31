a90bae405937fb89d33657ab8e6fb2f2c5f1cac8
ChickenchickenLove <ojt90902@naver.com>
2023-10-31 09:56:17 +0900

    easy 1 with java

--- src/main/java/org/example/easy/lc121/Solution.java
+++ src/main/java/org/example/easy/lc121/Solution.java
@@ -1,7 +1,14 @@
-package org.example.easy;
+package org.example.easy.lc121;
 
 class Solution {
     public int maxProfit(int[] prices) {
+        int result = 0;
+        int mv = Integer.MAX_VALUE;
 
+        for (int p : prices) {
+            mv = Math.min(p, mv);
+            result = Math.max(p - mv, result);
+        }
+        return result;
     }
 }
